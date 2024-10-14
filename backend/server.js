615b8f31989264216adac99483ef888b753a6722
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-10-14 12:40:53 +0200

    deployment configurations

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import noteRoutes from './routes/note.route.js'
@@ -8,11 +9,20 @@ dotenv.config();
 
 const app=express();
 
-const PORT=process.env.PORT || 5000
+const PORT=process.env.PORT || 5000;
+
+const __dirname= path.resolve();
 
 app.use(express.json()); //middleware(function that runs before you send the response back to the client) allows us to accept JSON data in the req.body
 
 app.use("/api/notes", noteRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, ()=>{
     connectDB();

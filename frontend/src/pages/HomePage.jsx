bfd93348a2888c1e08bc44b9a10e097fba5c224a
Bahati Philemon <bahatiphilmon8@gmail.com>
2024-10-15 10:28:02 +0200

    add search form

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
-import { Container, VStack, Text, Image, SimpleGrid} from "@chakra-ui/react";
+import { Container, VStack, Text, Image, SimpleGrid,FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText, } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNoteStore } from "../../store/note";
 import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
-  const {fetchNotes, notes}=useNoteStore();
+  const { fetchNotes, notes } = useNoteStore();
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchNotes();
   }, [fetchNotes]);
 
-  //console.log("notes", notes)
+  console.log("notes", notes)
 
   return (
     <Container maxW="container.xl" mt={10}>
@@ -26,50 +29,52 @@ const HomePage = () => {
           Current Ideas
         </Text>
 
-      	<SimpleGrid
-					columns={{
-						base: 1,
-						md: 2,
-						lg: 3,
-					}}
-					spacing={10}
-					w={"full"}
-				>
-         {
-          notes.map((note) => (
-            <NoteCard key={note._id} note={note}/>
-          ))
-         }
+        <FormControl>
+          <FormLabel>Search Note</FormLabel>
+          <Input type="email" />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+          {notes.map((note) => (
+            <NoteCard key={note._id} note={note} />
+          ))}
         </SimpleGrid>
 
         {notes.length === 0 && (
-           <>
-           <Text
-           fontSize="xl"
-           textAlign={"center"}
-           fontWeight="bold"
-           color="gray.500"
-         >
-           No ideas found 😢{" "}
-           <Link to={"/create"}>
-             <Text
-               as="span"
-               color="green.500"
-               _hover={{ textDecoration: "underline" }}
-             >
-               Create an idea
-             </Text>
-           </Link>
-         </Text>
+          <>
+            <Text
+              fontSize="xl"
+              textAlign={"center"}
+              fontWeight="bold"
+              color="gray.500"
+            >
+              No ideas found 😢{" "}
+              <Link to={"/create"}>
+                <Text
+                  as="span"
+                  color="green.500"
+                  _hover={{ textDecoration: "underline" }}
+                >
+                  Create an idea
+                </Text>
+              </Link>
+            </Text>
             <Image
-            src="https://img.freepik.com/free-vector/hand-drawn-flat-design-overwhelmed-people-illustration_23-2149352794.jpg?ga=GA1.1.990392183.1728665351&semt=ais_hybrid"
-            alt="Dan Abramov"
-          />
-           </>
- 
+              src="https://img.freepik.com/free-vector/hand-drawn-flat-design-overwhelmed-people-illustration_23-2149352794.jpg?ga=GA1.1.990392183.1728665351&semt=ais_hybrid"
+              alt="Dan Abramov"
+            />
+          </>
         )}
-
-             </VStack>
+      </VStack>
     </Container>
   );
 };
